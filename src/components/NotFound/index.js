68564bc95a4b8fe4db31b70117f1d27fc23a1284
Brandon Mellus <mellusbrandon@gmail.com>
2017-12-05 00:14:23 -0500

    A little more style

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -6,7 +6,7 @@ import './style.scss'
 
 const NotFound = () => (
   <div className='NotFound'>
-    <h2>{NOT_FOUND_HEADING}</h2>
+    <h2 className='text-danger NotFound-heading'>{NOT_FOUND_HEADING}</h2>
     <p>The requested document could not be found</p>
   </div>
 )
