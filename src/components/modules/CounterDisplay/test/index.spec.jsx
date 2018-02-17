840d51b01dcc4a2d8c26a7ccde0a36a180aa376b
Brandon Mellus <mellusbrandon@gmail.com>
2018-02-17 14:54:15 -0500

    Create CounterDisplay component

---
+++ src/components/modules/CounterDisplay/test/index.spec.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import CounterDisplay from '../../CounterDisplay';
+
+describe('<CounterDisplay />', () => {
+  const props = {
+    counter: 0,
+  };
+
+  it('renders', () => {
+    const wrapper = shallow(<CounterDisplay {...props} />);
+    expect(wrapper.hasClass('counter-display')).to.equal(true);
+  });
+
+  it('displays number', () => {
+    const wrapper = shallow(<CounterDisplay {...props} />).render();
+    expect(wrapper.text()).to.equal('0');
+  })
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+
 import messageReducer from './Message';
+import counterReducer from './Counter';
 
 export default combineReducers({
   message: messageReducer,
+  counter: counterReducer,
 });
