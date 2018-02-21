55fcb69973406a2e711f39617e6c02b0c24f1715
Brandon Mellus <mellusbrandon@gmail.com>
2018-02-21 02:12:22 +0000

    Create CounterInput component

---
+++ src/components/modules/CounterInput/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Container } from 'semantic-ui-react';
+
+import CounterSpinner from '../CounterSpinner';
+import CounterButton from '../CounterButton';
+
+import './style.scss';
+
+const CounterInput = ({ spinnerVal, incrementCounter, decrementCounter, onSpinnerChange }) => (
+    <Container className="counter-input">
+        <CounterSpinner spinnerVal onSpinnerChange />
+        <CounterButton spinnerVal onClick={incrementCounter} color="green" />
+        <CounterButton spinnerVal onClick={decrementCounter} color="red" />
+    </Container>
+);
+
+export default CounterInput;
+
+CounterInput.propTypes = {
+    spinnerVal: PropTypes.number.isRequired,
+    incrementCounter: PropTypes.func.isRequired,
+    decrementCounter: PropTypes.func.isRequired,
+    onSpinnerChange: PropTypes.func.isRequired,
+};
---
+++ src/components/modules/CounterInput/test/index.spec.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import CounterInput from '../../CounterInput';
+
+describe('<CounterInput />', () => {
+    const props = {
+        spinnerVal: 1,
+        incrementCounter: () => {},
+        decrementCounter: () => {},
+        onSpinnerChange: () => {},
+    };
+    
+    it('renders', () => {
+        const wrapper = shallow(<CounterInput {...props} />);
+        expect(wrapper.hasClass('counter-input')).to.equal(true);
+    });
+});
