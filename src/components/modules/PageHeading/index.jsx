8877a1f3e1899b16d1fb305ca49c8ab203de8f40
Brandon Mellus <mellusbrandon@gmail.com>
2018-02-23 11:33:15 -0500

    Create PageHeading component

---
+++ src/components/modules/PageHeading/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Header } from 'semantic-ui-react';
+
+import './style.scss';
+
+const PageHeading = () => (
+  <Header className="page-heading" as="h1">
+    Mels React Boilerplate
+  </Header>
+);
+
+export default PageHeading;
